d9a0d7f664e914632aea8f1fc374978becdf5889
George <gekosmadakis@gmail.com>
2024-09-26 20:44:39 +0300

    Added CSS to the styles.css

--- movierama/angularclient/src/app/app.component.ts
+++ movierama/angularclient/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { AddMovieDialogComponent } from './add-movie-dialog/add-movie-dialog.com
 import { MatDialog } from '@angular/material/dialog';
 import { AddLoginDialogComponent } from './add-login-dialog/add-login-dialog.component';
 import { AddSignUpDialogComponent } from './add-sign-up-dialog/add-sign-up-dialog.component';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ export class AppComponent {
   title = 'MovieRama';
   isUserLoggedIn: boolean = false;
   username: string = '';
+  currentUser: User[] = [];
 
   constructor(private loginService: LoginService, public dialog: MatDialog) {}
 
@@ -24,6 +26,16 @@ export class AppComponent {
     this.loginService.username.subscribe((username: string) => {
       this.username = username; // Update the value when it changes
       });
+      // const userJson = localStorage.getItem('currentUser');
+      // this.currentUser = userJson !== null ? JSON.parse(userJson) : null;
+      // console.log('User logged in ', this.currentUser);
+      // if(this.currentUser != null){
+      //   this.isUserLoggedIn = true;
+      //   //this.username = this.currentUser.username;
+      //   }
+      //   else {
+      //   this.isUserLoggedIn = false;
+      //   }
   }
   openLoginDialog(): void {
     const dialogRef = this.dialog.open(AddLoginDialogComponent, {
